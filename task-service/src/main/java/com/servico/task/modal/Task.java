6e30beb5db35a2a4ea3c6ecfbbd9283b0d3ac677
Rafael Yudi <rafasyudi@gmail.com>
2025-05-02 16:27:15 -0300

    modificação para criação do service-task;

---
+++ task-service/src/main/java/com/servico/task/modal/Task.java
@@ -0,0 +1,14 @@
+package com.servico.task.modal;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Task {
+
+}
