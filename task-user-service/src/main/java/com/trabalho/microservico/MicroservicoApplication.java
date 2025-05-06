85450c2821bd5b0aa414b50f5a317a66110022b8
Rafael Yudi <rafasyudi@gmail.com>
2025-05-06 15:37:08 -0300

    criação do submission service, definição do terceiro banco de dados e inicio da implementação.

--- task-user-service/src/main/java/com/trabalho/microservico/MicroservicoApplication.java
+++ task-user-service/src/main/java/com/trabalho/microservico/MicroservicoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-
+@EnableFeignClients
 public class MicroservicoApplication {
 
     public static void main(String[] args) {
